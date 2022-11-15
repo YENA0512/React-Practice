db8e7ce4dd6c30b064ca1baa0ff8d1b64fbb6962
egoing <egoing@gmail.com>
2022-11-15 13:18:16 +0900

    컴포넌트 화

--- 1115/app/src/App.js
+++ 1115/app/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+function Header(){
+  return <header>
+    <h1><a href="index.html">WEB</a></h1>
+  </header>
+}
+function Nav(){
+  return <nav>
+    <ul>
+        <li><a href="1.html">html</a></li>
+        <li><a href="2.html">css</a></li>
+        <li><a href="3.html">js</a></li>
+    </ul>
+  </nav>
+}
+function Article(){
+  return <article>
+    <h2>WELCOME</h2>
+    Hello, WEB
+  </article>
+}
 function App() {
   return (
     <div className="App">
-      Hi, REACT!
+      <Header />
+      <Nav />
+      <Article />
     </div>
   );
 }

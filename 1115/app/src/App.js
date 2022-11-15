a84cc59b193361d5e9a356e643f4c2f78a7b3c15
egoing <egoing@gmail.com>
2022-11-15 14:51:23 +0900

    목록을 컴포넌트에서 표현하기

--- 1115/app/src/App.js
+++ 1115/app/src/App.js
@@ -6,12 +6,11 @@ const Header = ({title})=>{
     <h1><a href="index.html">{title}</a></h1>
   </header>
 }
-const Nav = ()=>{
+const Nav = ({topics})=>{
+  const liTag = topics.map((t)=><li key={t.id}><a href={"/read/"+t.id}>{t.title}</a></li>);
   return <nav>
     <ul>
-        <li><a href="1.html">html</a></li>
-        <li><a href="2.html">css</a></li>
-        <li><a href="3.html">js</a></li>
+        {liTag}
     </ul>
   </nav>
 }
@@ -22,10 +21,15 @@ const Article = ({title, body})=>{
   </article>
 }
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+  ]
   return (
     <div className="App">
-      <Header title="웹"/>
-      <Nav/>
+      <Header title="웹" />
+      <Nav topics={topics} />
       <Article title="어서오세요!" body="웹의 세계로 초대합니다."/>
       {/* <ReactPlayer url='https://www.youtube.com/watch?v=7fy1eFEkrpU' width={640} /> */}
     </div>

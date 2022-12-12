64659a3273050fc63f3fce00504f1121777c34f9
Yena Lee <lmonari71@gmail.com>
2022-12-13 02:24:17 +0900

    feat: component 추가
    button, header, 공통 Styling 추가

--- emotion-diary/myapp/src/App.js
+++ emotion-diary/myapp/src/App.js
@@ -4,19 +4,48 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("left click")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("right click")}
+            />
+          }
+        />
         <h2>Hello</h2>
+
+        <MyButton
+          text={"Button"}
+          onClick={() => alert("button click")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"Button"}
+          onClick={() => alert("button click")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"Button"}
+          onClick={() => alert("button click")}
+          type={"default"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
---
+++ emotion-diary/myapp/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //...
+
+  // add daisyUI plugin
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake", "dracula", "cmyk"],
+  },
+};
